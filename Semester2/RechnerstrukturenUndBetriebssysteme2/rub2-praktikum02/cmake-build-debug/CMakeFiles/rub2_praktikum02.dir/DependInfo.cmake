08c59c89b6e3807b3550365bc6dcdf3633c542b2
LoweloDev <tobias-barthold@outlook.de>
2021-07-11 20:15:00 +0200

    rub2-praktikum02, rub2-uebung02, pk1-praktikum09

---
+++ Semester2/RechnerstrukturenUndBetriebssysteme2/rub2-praktikum02/cmake-build-debug/CMakeFiles/rub2_praktikum02.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/lowelodev/IdeaProjects/University/Semester2/RechnerstrukturenUndBetriebssysteme2/rub2-praktikum02/dateiinfo.c" "/home/lowelodev/IdeaProjects/University/Semester2/RechnerstrukturenUndBetriebssysteme2/rub2-praktikum02/cmake-build-debug/CMakeFiles/rub2_praktikum02.dir/dateiinfo.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Semester2/RechnerstrukturenUndBetriebssysteme2/rub2-praktikum02/cmake-build-debug/CMakeFiles/rub2_praktikum02.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rub2_praktikum02.dir/dateiinfo.c.o"
+  "rub2_praktikum02"
+  "rub2_praktikum02.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/rub2_praktikum02.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
